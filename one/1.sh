360653d0476e6cbd4df751a4a94aaf2916d9fa77
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-02 03:50:50 +0500

    Update 1.sh

--- one/1.sh
+++ one/1.sh
@@ -1,1 +1,5 @@
-
+curl -X POST \
+  -H 'Content-type: application/json' \
+  -H 'auth-token: 930f78b1-71a0-4287-98c3-fea60a557b6a' \
+  --data '{ "fileUrl":"URL_TO_APP_FILE", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
+  https://api.testrigor.com/api/v1/apps/n7uByFDs4Enb3xS6u/retest

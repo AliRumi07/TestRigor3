71ba3b4bd430b9f1f59abd38eb01e34af9d0e0ec
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-02 03:11:04 +0500

    Update 5.sh

--- four/5.sh
+++ four/5.sh
@@ -1,1 +1,5 @@
-
+curl -X POST \
+  -H 'Content-type: application/json' \
+  -H 'auth-token: 94b60856-9c35-446b-b446-cf6b178f257a' \
+  --data '{ "fileUrl":"URL_TO_APP_FILE", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
+  https://api.testrigor.com/api/v1/apps/wXQs32Tq2KFHJBmxX/retest

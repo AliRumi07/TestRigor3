7c733c3f5cfeed945d81721b6c43880938e1f325
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-02 01:48:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import threading
 
 app = Flask(__name__)
 
-scripts = ['1.sh', '2.sh', '3.sh', '4.sh', '5.sh', '6.sh', '7.sh', '8.sh', '12.sh', '13.sh', '14.sh']
+scripts = ['one/1.sh', 'one/2.sh', 'one/3.sh', 'one/4.sh', 'one/5.sh', 'one/6.sh', 'one/7.sh', 'one/8.sh', 'one/9.sh', 'one/10.sh', 'one/11.sh', 'two/1.sh', 'two/2.sh', 'two/3.sh', 'two/4.sh', 'two/5.sh', 'two/6.sh', 'two/7.sh', 'two/8.sh', 'two/9.sh', 'two/10.sh', 'two/11.sh', 'three/1.sh', 'three/2.sh', 'three/3.sh', 'three/4.sh', 'three/5.sh', 'three/6.sh', 'three/7.sh', 'three/8.sh', 'three/9.sh', 'three/10.sh', 'three/11.sh', 'four/1.sh', 'four/2.sh', 'four/3.sh', 'four/4.sh', 'four/5.sh', 'four/6.sh', 'four/7.sh', 'four/8.sh', 'four/9.sh', 'four/10.sh', 'four/11.sh', 'five/1.sh', 'five/2.sh', 'five/3.sh', 'five/4.sh', 'five/5.sh', 'five/6.sh', 'five/7.sh', 'five/8.sh', 'five/9.sh', 'five/10.sh', 'five/11.sh', 'six/1.sh', 'six/2.sh', 'six/3.sh', 'six/4.sh', 'six/5.sh', 'six/6.sh', 'six/7.sh', 'six/8.sh', 'six/9.sh', 'six/10.sh', 'six/11.sh', 'seven/1.sh', 'seven/2.sh', 'seven/3.sh', 'seven/4.sh', 'seven/5.sh', 'seven/6.sh', 'seven/7.sh', 'seven/8.sh', 'seven/9.sh', 'seven/10.sh', 'seven/11.sh', 'eight/1.sh', 'eight/2.sh', 'eight/3.sh', 'eight/4.sh', 'eight/5.sh', 'eight/6.sh', 'eight/7.sh', 'eight/8.sh', 'eight/9.sh', 'eight/10.sh', 'eight/11.sh', 'nine/1.sh', 'nine/2.sh', 'nine/3.sh', 'nine/4.sh', 'nine/5.sh', 'nine/6.sh', 'nine/7.sh', 'nine/8.sh', 'nine/9.sh', 'nine/10.sh', 'nine/11.sh', 'ten/1.sh', 'ten/2.sh', 'ten/3.sh', 'ten/4.sh', 'ten/5.sh', 'ten/6.sh', 'ten/7.sh', 'ten/8.sh', 'ten/9.sh', 'ten/10.sh', 'ten/11.sh']
 script_status = {script: {'status': 'not started', 'last_run': None, 'return_code': None} for script in scripts}
 
 def run_scripts_continuously():

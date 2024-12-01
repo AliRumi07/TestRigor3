e8c13b3f6860850dd71ae78d0dd49389dda4ffcd
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-02 03:32:29 +0500

    Update 2.sh

--- nine/2.sh
+++ nine/2.sh
@@ -1,1 +1,5 @@
-
+curl -X POST \
+  -H 'Content-type: application/json' \
+  -H 'auth-token: f56f70d0-c8c8-4161-a5eb-eedf18c6824d' \
+  --data '{ "fileUrl":"URL_TO_APP_FILE", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
+  https://api.testrigor.com/api/v1/apps/Bb7cnpq68EzpcKN3u/retest

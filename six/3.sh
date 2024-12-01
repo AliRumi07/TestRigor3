325f18c87d574d68ecf45741be16b93a0a573897
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-02 04:51:22 +0500

    Update 3.sh

--- six/3.sh
+++ six/3.sh
@@ -1,1 +1,5 @@
-
+curl -X POST \
+  -H 'Content-type: application/json' \
+  -H 'auth-token: 44cca7c7-f4b3-4c90-b0d0-07a7db275007' \
+  --data '{ "fileUrl":"URL_TO_APP_FILE", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
+  https://api.testrigor.com/api/v1/apps/piaWYrH5d7XSrBHcA/retest

5c4ffc0d4e9f0eebc13a9147c73c1b2db238cec8
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-02 21:52:48 +0500

    Update 3.sh

--- three/3.sh
+++ three/3.sh
@@ -1,5 +1,5 @@
 curl -X POST \
   -H 'Content-type: application/json' \
   -H 'auth-token: f96ec49f-1de1-4388-8376-4d5c69b0f8db' \
-  --data '{ "fileUrl":"URL_TO_APP_FILE", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
+  --data '{ "fileUrl":"https://endtest-drive.s3.us-west-2.amazonaws.com/m0VCCoihtO3pGd49pmlszgwY3KO8R4/JuicyAds.apk", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
   https://api.testrigor.com/api/v1/apps/Et9gsHbpmmoKrTZ32/retest

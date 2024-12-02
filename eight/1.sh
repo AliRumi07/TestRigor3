91d849d4cb6c7d0cbabe84b201e082fae97c1eaa
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-02 21:28:32 +0500

    Update 1.sh

--- eight/1.sh
+++ eight/1.sh
@@ -1,5 +1,5 @@
 curl -X POST \
   -H 'Content-type: application/json' \
   -H 'auth-token: 401864e6-855b-4765-b19e-6c4364c657f8' \
-  --data '{ "fileUrl":"https://endtest-drive.s3.us-west-2.amazonaws.com/toptBu7x3YNW588KyT1UxY4cd2cJKB/Mystic_Realms__Echoes_of_the_Forgotten.apk", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
+  --data '{ "fileUrl":"https://endtest-drive.s3.us-west-2.amazonaws.com/m0VCCoihtO3pGd49pmlszgwY3KO8R4/JuicyAds.apk", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
   https://api.testrigor.com/api/v1/apps/LtjvFpHgS8pxSF8Nk/retest

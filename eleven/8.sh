ed684be7e4d3f97deae2b9ec4b1d2d395a5acc8c
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-02 23:45:35 +0500

    Update 8.sh

--- eleven/8.sh
+++ eleven/8.sh
@@ -1,1 +1,5 @@
-
+curl -X POST \
+  -H 'Content-type: application/json' \
+  -H 'auth-token: 92e3793b-3471-4e4e-9a2e-2373f65f38d2' \
+  --data '{ "fileUrl":"https://endtest-drive.s3.us-west-2.amazonaws.com/toptBu7x3YNW588KyT1UxY4cd2cJKB/Mystic_Realms__Echoes_of_the_Forgotten.apk", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
+  https://api.testrigor.com/api/v1/apps/5GztF8H4oWxgJ3JN3/retest

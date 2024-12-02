8739b77043f0a71268945c61179f1ab013c1bbba
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-02 20:57:04 +0500

    Update 7.sh

--- three/7.sh
+++ three/7.sh
@@ -1,1 +1,5 @@
-
+curl -X POST \
+  -H 'Content-type: application/json' \
+  -H 'auth-token: 692a1bc3-5852-4fd3-827b-9c3e40f9d926' \
+  --data '{ "fileUrl":"URL_TO_APP_FILE", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
+  https://api.testrigor.com/api/v1/apps/KNCNcncmbSpDN64Xx/retest

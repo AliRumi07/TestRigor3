6853ae2e08af93f2e1bc071232b86531d6b00303
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-02 20:40:39 +0500

    Update 11.sh

--- three/11.sh
+++ three/11.sh
@@ -1,1 +1,5 @@
-
+curl -X POST \
+  -H 'Content-type: application/json' \
+  -H 'auth-token: 3c2db3ef-66f3-40ac-8bc9-d79bead08241' \
+  --data '{ "fileUrl":"URL_TO_APP_FILE", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
+  https://api.testrigor.com/api/v1/apps/4nicLTMg6LK6uys2M/retest

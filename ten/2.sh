ea3eda1379b18822a089bf67636aadcecf91dbf3
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-02 21:49:59 +0500

    Update 2.sh

--- ten/2.sh
+++ ten/2.sh
@@ -1,5 +1,5 @@
 curl -X POST \
   -H 'Content-type: application/json' \
   -H 'auth-token: 4f9630aa-608d-41d1-be20-101a8813e4a9' \
-  --data '{ "fileUrl":"URL_TO_APP_FILE", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
+  --data '{ "fileUrl":"https://endtest-drive.s3.us-west-2.amazonaws.com/m0VCCoihtO3pGd49pmlszgwY3KO8R4/JuicyAds.apk", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
   https://api.testrigor.com/api/v1/apps/KTxFX6JAJHLpYjg4x/retest

82a502851dd26c9705fb408aaa7bc07c176e6d70
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-04 02:55:53 +0500

    Update 2.sh

--- three/2.sh
+++ three/2.sh
@@ -1,5 +1,5 @@
 curl -X POST \
   -H 'Content-type: application/json' \
   -H 'auth-token: 01ff29d9-ca49-422a-8f2e-cdc1e314bb19' \
-  --data '{ "fileUrl":"https://endtest-drive.s3.us-west-2.amazonaws.com/dYExfUywaYKt9RUhw7232IwbiKbc6g/Business_Finance_8.apk", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
+  --data '{ "fileUrl":"https://endtest-drive.s3.us-west-2.amazonaws.com/CPN1iDEuHrFEOq7T8su6sxZdLB9zII/Adskeeper.apk", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
   https://api.testrigor.com/api/v1/apps/cgrvuvKfqvRS4jzxF/retest

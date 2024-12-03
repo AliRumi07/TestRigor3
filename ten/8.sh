e46a16fab06a4ec94e4fcefb8930d642af67e03d
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-03 11:05:48 +0500

    Update 8.sh

--- ten/8.sh
+++ ten/8.sh
@@ -1,5 +1,5 @@
 curl -X POST \
   -H 'Content-type: application/json' \
   -H 'auth-token: b5e619a3-50db-40ef-bcf6-c4fc7444bf4b' \
-  --data '{ "fileUrl":"https://endtest-drive.s3.us-west-2.amazonaws.com/m0VCCoihtO3pGd49pmlszgwY3KO8R4/JuicyAds.apk", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
+  --data '{ "fileUrl":"https://endtest-drive.s3.us-west-2.amazonaws.com/i7YTiy9kJ8SftBYHClnSCMRtdyvNpw/Business_Finance_7.apk", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
   https://api.testrigor.com/api/v1/apps/vN9CgWrkySCXqKFX7/retest

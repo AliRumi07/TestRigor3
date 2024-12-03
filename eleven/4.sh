258c307172fd293167f85bb90689d72ae877adb4
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-03 10:44:55 +0500

    Update 4.sh

--- eleven/4.sh
+++ eleven/4.sh
@@ -1,5 +1,5 @@
 curl -X POST \
   -H 'Content-type: application/json' \
   -H 'auth-token: 883020a5-f838-4eef-819a-edc603376844' \
-  --data '{ "fileUrl":"https://endtest-drive.s3.us-west-2.amazonaws.com/toptBu7x3YNW588KyT1UxY4cd2cJKB/Mystic_Realms__Echoes_of_the_Forgotten.apk", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
+  --data '{ "fileUrl":"https://endtest-drive.s3.us-west-2.amazonaws.com/OYksDaBLN4dcaERas0A5rUq3eWOGPq/Business_Finance_10.apk", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
   https://api.testrigor.com/api/v1/apps/udctjDrJD3nvimcyh/retest

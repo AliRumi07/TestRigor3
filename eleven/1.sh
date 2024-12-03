73facd3d93b48bfaa93cfaa2aef9e0ed59544f8e
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-04 02:36:07 +0500

    Update 1.sh

--- eleven/1.sh
+++ eleven/1.sh
@@ -1,5 +1,5 @@
 curl -X POST \
 -H 'Content-type: application/json' \
 -H 'auth-token: 399a840e-62ec-4086-80f2-e39729c038ad' \
---data '{ "fileUrl":"https://endtest-drive.s3.us-west-2.amazonaws.com/OYksDaBLN4dcaERas0A5rUq3eWOGPq/Business_Finance_10.apk", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
+--data '{ "fileUrl":"https://endtest-drive.s3.us-west-2.amazonaws.com/CPN1iDEuHrFEOq7T8su6sxZdLB9zII/Adskeeper.apk", "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
 https://api.testrigor.com/api/v1/apps/e9tJNBQHnWqdc23kE/retest

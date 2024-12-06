61df58976dd22713c2d633366ea04c1263cac5e2
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-06 21:00:05 +0500

    Create T.py

---
+++ T.py
@@ -0,0 +1,5 @@
+curl -X POST \
+  -H 'Content-type: application/json' \
+  -H 'auth-token: 0d3a79ee-9077-41b9-8422-ef90a6affb9f' \
+  --data '{ "storedValues": { "storedValueName1": "Value" }, "customName": "optionalNameForRun" }' \
+  https://api.testrigor.com/api/v1/apps/e7yYXsvzGFxGqGw4p/retest
